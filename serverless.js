bea3cdd5dcdac7eecbd510cfd8be38c9a46346c9
Erlin <erlin@everymundo.com>
2020-03-23 21:00:37 -0400

    refactoring

--- serverless.js
+++ serverless.js
@@ -5,30 +5,26 @@ class CloudflareDNS extends Component {
   async default(inputs = {}) {
     const EMAIL = process.env.CF_EMAIL;
     const API_KEY = process.env.CF_KEY;
-    const ZONE_ID = process.env.CF_ZONE_ID;
     const { zone, name, type, content, proxied = true} = inputs;
     this.context.debug(`Starting CloudflareDNS Component.`);
     this.context.debug(`Finding DNS zone`);
-    const cf = cloudflare({ email:EMAIL, key:API_KEY });
-    this.state.zoneId = ZONE_ID;
-    if (!this.state.zoneId) {
-      const zones = await cf.zones.browse();
-      const { id } = zones.result.find(  zoneObj => zoneObj.name === zone);
-      id && (this.state.zoneId = id);
-    }
 
+    const cf = cloudflare({ email:EMAIL, key:API_KEY });
+    const zones = await cf.zones.browse();
+    const { id } = zones.result.find( zoneObj => zoneObj.name === zone);
+    id && (this.state.zoneId = id);
+    
     if (!this.state.zoneId) {
       throw new Error(`"${zone}" not found`);
     }
 
-    if (!this.state.recordId) {
-      const records = await cf.dnsRecords.browse(this.state.zoneId);
-      const record = records.result.find(record => record.name === name);    
-      record && (this.state.recordId = record.id);
-    }
+    const recordName = `${name}.${content}`;
+    const records = await cf.dnsRecords.browse(this.state.zoneId);
+    const record = records.result.find( record => record.name ===  recordName);    
+    this.state.recordId = record && record.id;
 
     if (!this.state.recordId) {
-      this.context.debug(`Creating DNS Record: ${name}`);
+      this.context.debug(`Creating dns record for : ${recordName}`);
       const record = await cf.dnsRecords.add(this.state.zoneId, {
         name,
         type,
@@ -37,15 +33,12 @@ class CloudflareDNS extends Component {
       });
       record && (this.state.recordId = record.result.id);
     } else {
-      const { result } = await cf.dnsRecords.read(
-        this.state.zoneId,
-        this.state.recordId
-      );
-      if (result.name === name && result.type === type && result.content === content && result.proxied === proxied)
+      const { result } = await cf.dnsRecords.read( this.state.zoneId,this.state.recordId );
+      if (result.name === recordName && result.type === type && result.content === content && result.proxied === proxied)
       {
-        this.context.debug(`Skipping unchanged DNS record: ${name}`);
+        this.context.debug(`Skipping unchanged DNS record: ${recordName}`);        
       } else {
-        this.context.debug(`Updating DNS Record: ${name}`);
+        this.context.debug(`Updating DNS Record: ${recordName}`);
         await cf.dnsRecords.edit(this.state.zoneId, this.state.recordId, {
           name,
           type,
@@ -53,35 +46,22 @@ class CloudflareDNS extends Component {
           proxied        
         });
       }
+      
     }
     await this.save();    
     return { name: name };
-  }
+  };
 
-  async remove(inputs = {}) {
+  async remove() {
     const EMAIL = process.env.CF_EMAIL;
     const API_KEY = process.env.CF_KEY;
-    const { zone,  name} = inputs;
-    this.context.debug(`Removing DNS Record: ${name}`);
+    this.context.debug(`Removing DNS Record`);
     const cf = cloudflare({ email:EMAIL, key:API_KEY });
-    
     if (!this.state.zoneId) {
-      const zones = await cf.zones.browse();     
-      const { id } = zones.result.find(  zoneObj => zoneObj.name === zone);
-      id && (this.state.zoneId = id);
-    }
-
-    if (!this.state.zoneId) {
-      throw new Error(`No zone named "${zone}" found`);
-    }
-
+      throw new Error(`No zone found`);
+    }    
     if (!this.state.recordId) {
-      const records = await cf.dnsRecords.browse(this.state.zoneId);
-      const record = records.result.find(record => record.name === name);
-      record && (this.state.recordId = record.id);
-    }
-    if (!this.state.recordId) {
-      throw new Error(`No record "${name}" found`);
+      throw new Error(`No record found`);
     }
 
     await cf.dnsRecords.del(this.state.zoneId, this.state.recordId);
